af655311b9eec29c00733799b5c84f9616a77ec9
EmanuelFadda <emanuel.fadda@hotmail.com>
2022-12-28 18:39:00 +0100

    Creazione di un nome di default quando l'utente non ha un nome suo

--- Client/app/src/main/java/com/example/progettopokeapi/SettingActivity.java
+++ Client/app/src/main/java/com/example/progettopokeapi/SettingActivity.java
@@ -1,8 +1,15 @@
 package com.example.progettopokeapi;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -11,20 +18,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.progettopokeapi.pokedex.ListActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class SettingActivity extends AppCompatActivity {
 
 
     BottomNavigationView nav;
 
+    //activity volta alla modifica e/o visione dei dati dell'account del client, questi dati verranno
+    // utilizzati per giocare in partità, per questo l'applicazione deve verificare la presenza di
+    // un nome e un immagine , e nel caso in cui non siano inserite (per qualsiasi motivo)
+    // inserire immagine e/o nome di default. Questo accadrà anche al primo accesso all'applicazione.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
         nav=findViewById(R.id.navbarPok);
-
         nav.setSelectedItemId(R.id.settings);
-
         nav.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -48,5 +61,28 @@ public class SettingActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        //si ricerca il nome utente dell'account nella cartella shared_prefs
+        //SharedPreferences
+        EditText nameInput=findViewById(R.id.accountNameInput);
+        SharedPreferences shared_prefs=this.getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=shared_prefs.edit();
+        if(!shared_prefs.contains("nameAccount")){
+            editor.putString("nameAccount","bro");
+            editor.apply();
+        }
+        String name=shared_prefs.getString("nameAccount","ThereIsNoNameInSharedPrefs");
+        nameInput.setText(name);
+        //si ricerca nello storage interno dell'applicazione l'immagine del profilo;
+        /*FileInputStream fis=null;
+        try { //try-catch che verifica l'esistenza del file inerente all'immagine del profilo
+            fis=new FileInputStream("imageAccount.*");
+        } catch (FileNotFoundException e) {  //nel caso in cui non ci sia il l'applicazione ne salva uno di default
+
+        }
+        ImageView imgAccount=findViewById(R.id.accountImgView);
+        Bitmap imageAccount= BitmapFactory.decodeStream(fis);
+        imgAccount.setImageBitmap(imageAccount);*/
+
     }
 }
