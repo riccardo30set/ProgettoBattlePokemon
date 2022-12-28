30f2315154eed2c89cee16698ebef356a5d3f0d1
riccardo30set <despositoriccardo@istitutocorni.it>
2022-12-28 20:49:27 +0100

    Modifiche activity_setting.xml perchè fadda non sa fare nulla da solo :), ma è pur sempre un bravo ragazzo

--- Client/app/src/main/java/com/example/progettopokeapi/PartitaOnlineActivity.java
+++ Client/app/src/main/java/com/example/progettopokeapi/PartitaOnlineActivity.java
@@ -1,12 +1,19 @@
 package com.example.progettopokeapi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.progettopokeapi.pokedex.ListActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PartitaOnlineActivity extends AppCompatActivity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partita_online);
