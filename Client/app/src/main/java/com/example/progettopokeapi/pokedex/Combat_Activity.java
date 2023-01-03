a0feeb46baa7300f0eb306a36fc9dc1081fbb556
MazzoHS <marealmarco@gmail.com>
2023-01-03 15:23:11 +0100

    agggiute hashmap per la gestione delle statistiche di tutti e 6 i pokemon

--- Client/app/src/main/java/com/example/progettopokeapi/pokedex/Combat_Activity.java
+++ Client/app/src/main/java/com/example/progettopokeapi/pokedex/Combat_Activity.java
@@ -1,5 +1,7 @@
 package com.example.progettopokeapi.pokedex;
 
+import static java.util.Map.entry;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,15 +9,39 @@ import android.view.WindowManager;
 
 import com.example.progettopokeapi.R;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Combat_Activity extends AppCompatActivity {
-    HashMap<String,Integer>  player;
+    List<Map<String, Integer>> pokemonsPlayer = new ArrayList<Map<String, Integer>>();
+    //List<Map<String, Integer>> pokemonsEnemy = new ArrayList<Map<String, Integer>>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_combat);
+        for(int i=0;i<6;i++){
+            Map<String, Integer> pokemonPlayer = new HashMap<String, Integer>() {{
+                put("HP", 0);
+                put("ATK", 0);
+                put("DEF", 0);
+                put("SPATK", 0);
+                put("SPDEF", 0);
+                put("SPEED", 0);
+            }};
+            pokemonsPlayer.add(pokemonPlayer);
+            /*Map<String, Integer> pokemonEnemy = new HashMap<String, Integer>() {{
+                put("HP", 0);
+                put("ATK", 0);
+                put("DEF", 0);
+                put("SPATK", 0);
+                put("SPDEF", 0);
+                put("SPEED", 0);
+            }};*/
+            //pokemonsEnemy.add(pokemonPlayer);
+        }
     }
     public int CalcoloHP(int base, int EV, int IV, int level){
         return (int) (Math.floor(0.01*(2*base+IV+Math.floor(0.25*EV))*level)+level+10);
@@ -23,4 +49,5 @@ public class Combat_Activity extends AppCompatActivity {
     public int CalcoloPureStats(int base, int EV, int IV, int level){
         return  (int) (Math.floor(0.01*(2*base+IV+Math.floor(0.25*EV))*level)+5);
     }
+
 }
