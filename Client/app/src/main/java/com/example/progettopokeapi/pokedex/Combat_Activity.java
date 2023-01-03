26b0a16776f81335b9bc7bbc66a8ec83bb5910a8
MazzoHS <marealmarco@gmail.com>
2023-01-03 14:00:29 +0100

    aggiunte formule di calcolo delle statiche

--- Client/app/src/main/java/com/example/progettopokeapi/pokedex/Combat_Activity.java
+++ Client/app/src/main/java/com/example/progettopokeapi/pokedex/Combat_Activity.java
@@ -7,12 +7,20 @@ import android.view.WindowManager;
 
 import com.example.progettopokeapi.R;
 
-public class Combat_Activity extends AppCompatActivity {
+import java.util.HashMap;
 
+public class Combat_Activity extends AppCompatActivity {
+    HashMap<String,Integer>  player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_combat);
+    }
+    public int CalcoloHP(int base, int EV, int IV, int level){
+        return (int) (Math.floor(0.01*(2*base+IV+Math.floor(0.25*EV))*level)+level+10);
+    }
+    public int CalcoloPureStats(int base, int EV, int IV, int level){
+        return  (int) (Math.floor(0.01*(2*base+IV+Math.floor(0.25*EV))*level)+5);
     }
 }
