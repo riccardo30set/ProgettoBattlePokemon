20d755b79d28f7de9b862304979e72363cd1fe28
Sighi <filippoandreasighinolfi@gmail.com>
2023-01-18 04:35:20 +0100

    cambiato ip

--- Client/app/src/main/java/com/example/progettopokeapi/MainActivity.java
+++ Client/app/src/main/java/com/example/progettopokeapi/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickOnline(View view){
-        Client.connect(this,"10.10.10.189",1500);
+        Client.connect(this,"10.13.13.2",1500);
         String gameName = "Partita di "+Client.shared_prefs.getString("nameAccount","Giocatore");
         Client.createGame(gameName,findViewById(R.id.opponentName));
         Intent online = new Intent(getApplicationContext(), PartitaOnlineActivity.class);
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onClickPartecipa(View view){
-        Client.connect(this,"10.10.10.189",1500);
+        Client.connect(this,"10.13.13.2",1500);
         startActivity(new Intent(this, Partecipa.class));
     }
 
