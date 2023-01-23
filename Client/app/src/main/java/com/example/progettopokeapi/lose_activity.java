06c74c484637ef2dfe993089d086ccc63e28c808
riccardo30set <despositoriccardo@istitutocorni.it>
2023-01-23 19:52:28 +0100

    Modifica sulla vittoria e sconfitta nel gioco

--- Client/app/src/main/java/com/example/progettopokeapi/lose_activity.java
+++ Client/app/src/main/java/com/example/progettopokeapi/lose_activity.java
@@ -1,14 +1,44 @@
 package com.example.progettopokeapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import nl.dionsegijn.konfetti.core.PartyFactory;
+import nl.dionsegijn.konfetti.core.Position;
+import nl.dionsegijn.konfetti.core.emitter.Emitter;
+import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
+import nl.dionsegijn.konfetti.core.models.Shape;
+import nl.dionsegijn.konfetti.xml.KonfettiView;
 
 public class lose_activity extends AppCompatActivity {
 
+    private KonfettiView konfettiView = null;
+    private Shape.DrawableShape drawableShape = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lose);
+
+
+        final Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_heart);
+        drawableShape = new Shape.DrawableShape(drawable, true);
+
+        konfettiView = findViewById(R.id.konfettiviewlos);
+        EmitterConfig emitterConfig = new Emitter(200L, TimeUnit.MILLISECONDS).max(100);
+        konfettiView.start(
+                new PartyFactory(emitterConfig)
+                        .spread(1280)
+                        .shapes(Arrays.asList(Shape.Square.INSTANCE, Shape.Circle.INSTANCE, drawableShape))
+                        .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                        .setSpeedBetween(0f, 30f)
+                        .position(new Position.Relative(0.5, 0.3))
+                        .build()
+        );
     }
 }
